7e86d4d4a49792b4ec4fbf8583e97e0bcf0e393a
Julie <jawarren89@gmail.com>
2022-06-21 15:21:19 -0700

    fixes TaskList destructured prop. Lifts isComplete state to App.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Task from './Task';
 import './TaskList.css';
 
-const TaskList = ({ tasks }) => {
+const TaskList = ({ tasks, onUpdateTask }) => {
   const getTaskListJSX = (tasks) => {
     return tasks.map((task) => {
       return (
@@ -12,7 +12,7 @@ const TaskList = ({ tasks }) => {
           id={task.id}
           title={task.title}
           isComplete={task.isComplete}
-          onUpdateTask={task.onUpdateTask}
+          onUpdateTask={onUpdateTask}
         />
       );
     });
